8fa61b2645a1c0b62066b139a6ebf0c8db327aa3
SamahjBerry <samahjberry12@gmail.com>
2025-03-04 18:26:41 +0000

    Pending changes exported from your codespace

---
+++ Tables.py
@@ -0,0 +1,14 @@
+O = 'open'
+X = 'taken'
+tables = { 
+    "2": {
+        "T1": [O,O,X,O,O,O], #Each table has 6 different time slots
+        "T6": [O,O,O,X,O,X],
+        "T3": [O,O,O,O,O,O]
+}
+    "4": {
+        "T5": [O,O,O,O,X,X],
+        "T2": [O,O,O,O,O,O]
+
+    }#the tables with said number of seats
+}
